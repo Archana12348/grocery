a15c52b508717b0d678e9c5a1a9bea6f750f82d1
pwtmuskansharma <pwtmuskansharma@gmail.com>
2025-11-15 17:14:05 +0530

    product detail is completed with /product without id

--- grocery/src/frontend/frontendroutes.js
+++ grocery/src/frontend/frontendroutes.js
@@ -3,11 +3,13 @@ import FrontLayout from "./layout/FrontLayout";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Contact from "./pages/Contact";
+import ProductPage from "./pages/ProductDetail";
 
 export const FrontendRoutes = (
   <Route path="/" element={<FrontLayout />}>
     <Route index element={<Home />} />
     <Route path="shop" element={<Shop />} />
     <Route path="contact" element={<Contact />} />
+    <Route path="product" element={<ProductPage />} />
   </Route>
 );
